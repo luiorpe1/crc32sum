e99f3c92207f53a6d92d4394fc132ba0b55f7e85
Luis Ortega Pérez de Villar <luiorpe1@upv.es>
2013-11-15 19:10:31 +0100

    crc32sum: Fix warnings and some housekeeping

--- crc32sum.c
+++ crc32sum.c
@@ -18,7 +18,7 @@
 void usage()
 {
 	printf("Usage: %s [FILE]...\n"
-	       "Print CRC (32-bit) checksums."
+	       "Print CRC (32-bit) checksums.",
 	       PROGRAM_NAME);
 }
 
@@ -31,13 +31,11 @@ int digest_filestream(int fd)
 	ssize_t ret;
 	size_t len = 4096; /* 4KB */
 
-	//char *buf = calloc(4096, sizeof(char));
+	/* char *buf = calloc(4096, sizeof(char)); */
 	char *buf = malloc(4096);
 
-	if (buf == NULL) {
-		//perror("calloc");
+	if (buf == NULL)
 		return 1;
-	}
 
 	uLong crc = crc32(0L, Z_NULL, 0);
 
@@ -45,11 +43,10 @@ int digest_filestream(int fd)
 		if (ret == -1) {
 			if (errno == EINTR)
 				continue;
-			//perror("read");
 			break;
 		}
 
-		crc = crc32(crc, buf, ret);
+		crc = crc32(crc, (Bytef *) buf, ret);
 	}
 
 	free(buf);
@@ -64,59 +61,42 @@ int digest_file(const char *filename)
 {
 	int fd;
 	struct stat sb;
-	char *data; 
+	char *data;
 	uLong crc;
-	
+
 	/* Open file */
 	/* To open files >2GB in 32bit systems look at "man 2 open"
 	 * and search for O_LARGEFILE */
 	fd = open(filename, O_RDONLY);
-	if (fd == -1) {
-		//fprintf(stderr, "Unable to open %s\n", filename);
-		
-		//fprintf(stderr, "%s: %s [error code: %d]\n",
-		//	filename, strerror(errno), errno);
-		//perror(filename);
-		
+	if (fd == -1)
 		return 1;
-	}
 
 	/* Get file information and perform checks */
-	if (fstat(fd, &sb) == -1) {
-		//fprintf(stderr,
-		//	"%s: Unable to retrieve information\n", filename);
-		return 1; 
-	}
+	if (fstat(fd, &sb) == -1)
+		return 1;
 
-	if (!S_ISREG (sb.st_mode)) {
-		//fprintf(stderr, "%s is not a file\n", filename);
+	if (!S_ISREG(sb.st_mode)) {
 		errno = EINVAL;
 		return 1;
 	}
 
 	/* Map file into memory */
 	data = mmap(0, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
-	if (data == MAP_FAILED) {
-		//fprintf(stderr, "unable to map %s\n", filename);
+	if (data == MAP_FAILED)
 		return 1;
-	}
 
-	//TODO Find out why "madvise" doesn't compile
-	//madvise(data, sb.st_size, MADV_SEQUENTIAL);
+	/* TODO Find out why "madvise" doesn't compile */
+	/* madvise(data, sb.st_size, MADV_SEQUENTIAL); */
 
-	/* Calculate checksum */
+	/* Compute checksum */
 	crc = crc32(0L, (Bytef *) data, sb.st_size);
 
 	/* Unmap and close file */
-	if (munmap(data, sb.st_size) == -1) {
-		//fprintf(stderr, "unable to unmap %s\n", filename);
+	if (munmap(data, sb.st_size) == -1)
 		return 1;
-	}
 
-	if (close(fd) == -1) {
-		//fprintf(stderr, "unable to close %s\n", filename);
+	if (close(fd) == -1)
 		return 1;
-	}
 
 	return crc;
 }
@@ -125,17 +105,14 @@ int digest_file(const char *filename)
 int main(int argc, char **argv)
 {
 	if (argc < 2) {
-		//fprintf(stderr, "usage: %s file1 [...]\n", argv[0]);
 		usage();
 		return 1;
 	}
 
-	char *crcname;
-
 	for (int i = 1; i < argc; i++) {
 		errno = 0;
 		uLong crc = digest_file(argv[i]);
-		
+
 		if (errno)
 			printf("%s: %s\n", argv[i], strerror(errno));
 		else
